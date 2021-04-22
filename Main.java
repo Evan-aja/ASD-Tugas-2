185a4b4fc71be59b39da19fadcfbcaf4342d9778
Evan <71018479+Evan-aja@users.noreply.github.com>
2021-04-22 13:27:45 +0700

    Melengkapi kerangka

--- Main.java
+++ Main.java
@@ -4,20 +4,38 @@ import java.util.Scanner;
 
 class Masuk {
 	
-	void ADD() {
+	//untuk menambah tag baru (ADD;div atau ADD;html dsb)
+	void ADD1(String selector,String namaTag,String namaId) {
 		
 	}
-	void PRINT() {
+	//untuk menambah TEXT kedalam sebuah id (ADD;TEXT:#id)
+	void ADD2(String TEXT/*untuk memanggil TEXT saja*/, String selector,String kalimat) {
 		
 	}
-	void DELETE() {
+	//untuk menambah konten didalam sebuah id (ADD;#html)
+	void ADD3(String namaId,String namaTag,String namaIdChild) {
 		
+	}
+	void PRINT(String selector,int jumlahLine) {
+		
+	}
+	void DELETE(String selector) {
+		boolean ada = SEARCH(selector);
+		if(ada==true) {
+			
+		}else{
+			
+		}
+	}
+	boolean SEARCH(String selector) {
+		
+		return false;
 	}
 }
 
 public class Main {
 	public static void main(String[] args) {
-//		Masuk learn=new Masuk();
+		Masuk learn=new Masuk();
 		Scanner scan = new Scanner(System.in);
 		String[] tugas=new String[2];
 		int i=0;
@@ -33,5 +51,22 @@ public class Main {
 			}
 		}
 		scan.close();
+		for(int j=0;j<tugas.length;j++) {
+			String[]words=tugas[j].trim().split(";");
+			if(words[0].equals("ADD")) {
+				if(words[1].substring(0,1).equals("#")) {
+					learn.ADD3(words[1],words[2],words[3]);
+				}else if(words[1].equals("TEXT")) {
+					learn.ADD2(words[1],words[2],words[3]);
+				}else {
+					learn.ADD1(words[1],words[2],words[3]);
+				}
+			}else if(words[0].equals("PRINT")) {
+				int jumlah=Integer.parseInt(words[2]);
+				learn.PRINT(words[1], jumlah);
+			}else if(words[0].equals("DELETE")) {
+				learn.DELETE(words[1]);
+			}
+		}
 	}
 }
